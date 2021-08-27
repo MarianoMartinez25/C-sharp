256a3f244c9bdd59fd5fdbcb4d8637353900081c
Mariano Martinez <55413946+MarianoMartinez25@users.noreply.github.com>
2021-08-27 16:36:08 -0300

    Add files via upload

---
+++ EstructuraRepetitivaFor2/EstructuraRepetitivaFor2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraRepetitivaFor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f, suma, promedio, valor;
+            string linea;
+            suma = 0;
+            for (f = 1; f <= 10; f++)
+            {
+                Console.Write("Ingrese valor: ");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                suma = suma + valor;
+            }
+            Console.Write("La suma es:");
+            Console.WriteLine(suma);
+            promedio = suma / 10;
+            Console.Write("El promedio es:");
+            Console.Write(promedio);
+            Console.ReadKey();
+        }
+    }
+}
